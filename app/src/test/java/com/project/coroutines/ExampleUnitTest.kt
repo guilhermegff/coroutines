70039fe043b99adc7075d4eb6071aee04842d644
Guilherme <guilherme.freire@actdgitial.com.br>
2022-04-07 14:12:40 -0300

    Initial commit

---
+++ app/src/test/java/com/project/coroutines/ExampleUnitTest.kt
@@ -0,0 +1,26 @@
+package com.project.coroutines
+
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import org.junit.Test
+
+import org.junit.Assert.*
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.coroutineContext
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        val a = runBlocking {
+                this.launch {
+
+                }
+        }
+    }
+}
